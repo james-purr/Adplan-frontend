1ef33500ff1bf70c0ff01f7901beb70bfe5e7ff9
james-purr <jamesbennett@purrdigital.com>
2018-07-16 11:35:41 +0100

    dropdown magic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 51.507, lng: 0.1278},
-          zoom: 10
-        });
-      }
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 51.507, lng: 0.1278},
+      zoom: 10
+    });
+  }
+
+$( document ).ready(function() {
+ 	$('span.checkmark').on('click', function(e){
+ 		e.preventDefault();
+		e.stopPropagation();
+		$(e.target).next().toggleClass('active');
+ 	});
+ });
